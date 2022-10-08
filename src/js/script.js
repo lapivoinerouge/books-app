4528ee3ba818ed08cde0517de64c341cc3628236
lapivoinerouge <wioladys@gmail.com>
2022-10-08 22:08:36 +0200

    Delegate liking event to books container

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,11 @@
     booksList: '.books-list',
   };
 
+  const classNames = {
+    bookImage: 'book__image',
+    favorite: 'favorite',
+  };
+
   const templates = {
     bookTemplate: Handlebars.compile(document.querySelector('#template-book').innerHTML),
   };
@@ -25,24 +30,44 @@
   }
 
   function initActions() {
-    const bookImages = document.querySelectorAll(select.bookImage);
+    // const bookImages = document.querySelectorAll(select.bookImage);
 
-    for (const bookImage of bookImages) {
+    const bookContainer = document.querySelector(select.booksList);
 
-      bookImage.addEventListener('dblclick', function() {
-        event.preventDefault();
+    bookContainer.addEventListener('dblclick', function() {
+      event.preventDefault();
+
+      if (event.target.offsetParent.classList.contains(classNames.bookImage)) {
+        const bookImage = event.target.offsetParent;
 
         const id = bookImage.getAttribute('data-id');
         if (favoriteBooks.indexOf(id) > -1) {
           const idx = favoriteBooks.indexOf(id);
           favoriteBooks.splice(idx, 1);
-          bookImage.classList.remove('favorite');
+          bookImage.classList.remove(classNames.favorite);
         } else {
           favoriteBooks.push(id);
-          bookImage.classList.add('favorite');
+          bookImage.classList.add(classNames.favorite);
         }
-      });
-    }
+      }
+    });
+
+    // for (const bookImage of bookImages) {
+
+    //   bookImage.addEventListener('dblclick', function() {
+    //     event.preventDefault();
+
+    //     const id = bookImage.getAttribute('data-id');
+    //     if (favoriteBooks.indexOf(id) > -1) {
+    //       const idx = favoriteBooks.indexOf(id);
+    //       favoriteBooks.splice(idx, 1);
+    //       bookImage.classList.remove('favorite');
+    //     } else {
+    //       favoriteBooks.push(id);
+    //       bookImage.classList.add('favorite');
+    //     }
+    //   });
+    // }
   }
 
   renderBooks();
